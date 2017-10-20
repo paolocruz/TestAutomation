d7a3b30aa805f9eea9d4d7c2d1748a4bacb542ba
Paolo Cruz <ct_rion@yahoo.com>
2017-10-20 16:46:48 +0800

    Update test-t0002: update print message

--- TestAutomation/MasterViewController.swift
+++ TestAutomation/MasterViewController.swift
@@ -25,7 +25,7 @@ class MasterViewController: UITableViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
-        print("test0001")
+        print("test0002")
     }
 
     override func viewWillAppear(_ animated: Bool) {
